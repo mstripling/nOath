fb015c85289c1c0399c0ebd1ab355e9e033213e5
Miles Stripling <miles.stripling@gmail.com>
2024-12-01 20:09:34 -0800

    some changes

--- util/post.go
+++ util/post.go
@@ -1,47 +1,47 @@
 package util
 
 import  (
-  "net/http"
-  "encoding/json"
-  "bytes"
-  "fmt"
+    "net/http"
+    "encoding/json"
+    "bytes"
+    "fmt"
 )
 
 func Post(req *http.Request, resp *http.Response, p *InboundPayload) (*http.Response, error) {
-  var refreshResp RefreshResponse
-  err := json.NewDecoder(resp.Body).Decode(&refreshResp)
-  if err != nil {
-    return nil, fmt.Errorf("Error marshalling JSON: %v", err)
-    }
-  reqBody, err := json.Marshal(p.Data)
-  if err != nil {
-    return nil, fmt.Errorf("Error marshalling JSON: %v", err)
+    var refreshResp RefreshResponse
+    
+    err := json.NewDecoder(resp.Body).Decode(&refreshResp)
+    if err != nil {
+        return nil, fmt.Errorf("Error marshalling JSON: %v", err)
     }
 
-  postReq, err := http.NewRequest(req.Method, p.Endpoint, bytes.NewReader(reqBody))
-  if err != nil {
-    return nil, fmt.Errorf("Error posting lead: %v", err)
-  }
+    reqBody, err := json.Marshal(p.Data)
+    if err != nil {
+        return nil, fmt.Errorf("Error marshalling JSON: %v", err)
+    }
 
-  
+    postReq, err := http.NewRequest(req.Method, p.Endpoint, bytes.NewReader(reqBody))
+    if err != nil {
+        return nil, fmt.Errorf("Error posting lead: %v", err)
+    }
 
-  // Copy all headers from original request to post request
-for key, values := range req.Header {
-    for _, value := range values {
-        if key == "Authorization" {
-            // Append the access token to the value of the Authorization header
-            value = value + " " + refreshResp.AccessToken
+    // Copy all headers from original request to post request
+    for key, values := range req.Header {
+        for _, value := range values {
+            if key == "Authorization" {
+                // Append the access token to the value of the Authorization header
+                value = value + " " + refreshResp.AccessToken
+            }
+            postReq.Header.Add(key, value)
         }
-        postReq.Header.Add(key, value)
     }
-}
 	
-  postResp, err := http.DefaultClient.Do(postReq)
-  if err != nil {
-    return postResp, err
-  }
+    postResp, err := http.DefaultClient.Do(postReq)
+    if err != nil {
+        return postResp, err
+    }
 
-  return postResp, nil
+    return postResp, nil
 }
 
 
--- util/refresh.go
+++ util/refresh.go
@@ -1,38 +1,75 @@
 package util
 
 import (
-  "net/http"
-  "errors"
+    "net/http"
+    "errors"
+    "io"
+    "encoding/json"
+    "fmt"
+    "bytes"
 )
 
 type InboundPayload struct {
-  Data map[string]interface{} `json:"data"`
-  Endpoint string `json:"endpoint"`
-  AuthServer string `json:"authserver"`
-  RefreshToken string `json:"refreshtoken"`
+    Data map[string]interface{} `json:"data"`
+    Endpoint string `json:"endpoint"`
+    AuthServer string `json:"auth_server"`
+    RefreshToken string `json:"refresh_token"`
+    Redirect bool `json:"redirect,omitempty"`
+    RefreshHeader map[string]interface{} `json:"refresh_header,omitempty"`
+    RefreshBody interface{} `json:"refresh_body,omitempty"`
+    AccessTokenLocation interface{} `json:"access_token_location,omitempty"`
 }
 
 type RefreshResponse struct {
-	AccessToken string `json:"access_token"`
-	ExpiresIn   int    `json:"expires_in"`
-	APIDomain   string `json:"api_domain"`
-	TokenType   string `json:"token_type"`
+    AccessToken string `json:"access_token"`
+    ExpiresIn   int    `json:"expires_in"`
+    APIDomain   string `json:"api_domain"`
+    TokenType   string `json:"token_type"`
 }
 
 func RefreshAuthToken(r *http.Request, p *InboundPayload) (*http.Response, error) {
-  if p.Endpoint == "" || p.AuthServer == "" || p.RefreshToken == "" {
-    return nil, errors.New("Missing endpoint, authserver, or refreshtoken")
-  }
+    if p.Endpoint == "" || p.AuthServer == "" || p.RefreshToken == "" {
+        return nil, errors.New("Missing endpoint, auth_server, or refresh_token")
+    }
 
-  refreshReq, err := http.NewRequest(r.Method, p.AuthServer, nil)  
-  if err != nil {
-    return nil, err
-  }
+    var bodyReader io.Reader
+    if p.RefreshBody != nil {
+        // Marshal RefreshBody to JSON if it contains data
+        bodyBytes, err := json.Marshal(p.RefreshBody)
+        if err != nil {
+            return nil, fmt.Errorf("failed to marshal refresh body: %w", err)
+        }
+        bodyReader = bytes.NewReader(bodyBytes) // Convert JSON bytes to io.Reader
+    }
 
-  refreshResp, err := http.DefaultClient.Do(refreshReq)
-  if err != nil {
-    return refreshResp, err
-  }
+    // Create the new HTTP request
+    refreshReq, err := http.NewRequest(r.Method, p.AuthServer, bodyReader)
+    if err != nil {
+        return nil, fmt.Errorf("failed to create new request: %w", err)
+    }
 
-  return refreshResp, nil
+    // Use RefreshHeader from payload if it exists
+    if len(p.RefreshHeader) > 0 {
+        for key, value := range p.RefreshHeader {
+            // Convert key and value to strings before adding to header
+            keyStr := key // it's already a string
+            valueStr := fmt.Sprintf("%v", value) // convert value to string using fmt.Sprintf
+            refreshReq.Header.Set(keyStr, valueStr)
+        }
+    } else {
+        // Fall back to headers from the original request if RefreshHeader is empty
+        for key, values := range r.Header {
+            for _, value := range values {
+                refreshReq.Header.Add(key, value)
+            }
+        }
+    }
+
+
+    refreshResp, err := http.DefaultClient.Do(refreshReq)
+        if err != nil {
+        return refreshResp, err
+    }
+
+    return refreshResp, nil
 }
